2a9a3291cf881579b7fd2b3c0516fcee4855cf62
laury bretteville <laury.bretteville@edhec.com>
2018-10-18 15:40:56 +0200

    back end done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,34 @@
 class DosesController < ApplicationController
+before_action :set_cocktail, only: [:new, :create]
+
+  def new
+    @dose = Dose.new
+    # @cocktail = @dose.cocktail
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(params_dose)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def params_dose
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+require "json"
+require "open-uri"
+require_relative "../app/models/ingredient"
+
+# TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# ONLY THE 10 FIRST
+drinks = open(url).read
+# on recupère les 30 valeurs {"stringredient1: "lihght rum"} de la key "drinks"
+ingredients_first = JSON.parse(drinks)["drinks"]
+ingredients_first.each do |ingredient_hash|
+  # post_id = post.to_i
+  create_fake_list = Ingredient.new(name: ingredient_hash["strIngredient1"])
+  create_fake_list.save
+end
